be475e39ab624229386fa9614062e73aac71bd8b
JLoosa <JLoosa@github.com>
2021-04-22 18:02:51 -0600

    Slight modification to In-Band code. Moving to Out-Of-Band next

--- LabTCPNetworking/helloServerInBand.py
+++ LabTCPNetworking/helloServerInBand.py
@@ -1,32 +1,68 @@
 
-
-import socket, sys, re
+import socket, sys, re, os
 
 """
     Example helloServer (echo server) using In-Band Signaling with
     each message being terminated by a newline.
 """
 
+# Static variables
+_encoding = "utf-8"
+_address = ""  # Symbolic name meaning all available interfaces
+_port = 50001
+_recv_timeout = None
+_packet_size = 1024
+_stdin = 0
+_stdout = 1
+_allow_empty_packet = False
 
-listenPort = 50001
-listenAddr = ''       # Symbolic name meaning all available interfaces
+# Printing to Console
+def printf(msg: str, *args, fd: int = _stdout):
+    try:
+        os.write(fd, (msg % args).encode(encoding=_encoding))
+    except TypeError:
+        print(str)  # Fallback if os.write has an error
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((listenAddr, listenPort))
-s.listen(1)              # allow only one outstanding request
+# Method to send data
+def send_msg(msg: str, sock: socket):
+    sock.send((msg + "\n").encode(encoding=enc))
+
+# method to shut down a connection
+def close_connection(sock: socket):
+    if not sock:
+        return
+    sock.shutdown(socket.SHUT_WR)
+    sock.close()
+
+# Method to recieve data
+def recv_msg(sock: socket):
+    try:
+        return sock.recv(_packet_size).decode(encoding=_encoding)
+    except socket.timeout as timeout:
+        return ""
+    except socket.error as error:
+        printf("Connection recv error: %s\n" % error.strerror)
+        return ""
+
+localSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+localSocket.bind((_address, _port))
+localSocket.listen(1)              # allow only one outstanding request
 # s is a factory for connected sockets
 
-conn, addr = s.accept()  # wait until incoming connection request (and accept it)
-print('Connected by', addr)
-while 1:
-    data = conn.recv(1024).decode()
+conn, addr = localSocket.accept()  # wait until incoming connection request (and accept it)
+printf('Connected by %s\n', addr)
+while conn:
+    data = recv_msg(conn)
     if len(data) == 0:
-        print("Zero length read, nothing to send, terminating")
-        break
+        if not _allow_empty_packet:
+            printf("Connection has not sent any data, closing.\n")
+            close_connection(conn)
+            conn = None
+        continue
     sendMsg = "Echoing %s" % data
-    print("Received '%s', sending '%s'" % (data, sendMsg))
+    printf("Received '%s', sending '%s'\n" % (data, sendMsg))
     while len(sendMsg):
         bytesSent = conn.send(sendMsg.encode())
         sendMsg = sendMsg[bytesSent:0]
-conn.shutdown(socket.SHUT_WR)
-conn.close()
+close_connection(conn)
+printf("Connection Closed.\n")
